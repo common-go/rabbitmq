30cbb17c7fd6f74ca143abaa1bafddc586524a69
Duc Nguyen <minhduc140583@gmail.com>
2021-04-25 01:51:07 +0700

    rename producer to publisher

--- publisher.go
+++ publisher.go
@@ -7,24 +7,24 @@ import (
 	"github.com/streadway/amqp"
 )
 
-type Producer struct {
+type Publisher struct {
 	Channel      *amqp.Channel
 	ExchangeName string
 	Key          string
 	ContentType  string
 }
 
-func NewProducer(channel *amqp.Channel, exchangeName string, key string, contentType string) (*Producer, error) {
+func NewPublisher(channel *amqp.Channel, exchangeName string, key string, contentType string) (*Publisher, error) {
 	if len(key) == 0 {
 		key = "info"
 	}
 	if len(contentType) == 0 {
 		contentType = "text/plain"
 	}
-	return &Producer{Channel: channel, ExchangeName: exchangeName, Key: key, ContentType: contentType}, nil
+	return &Publisher{Channel: channel, ExchangeName: exchangeName, Key: key, ContentType: contentType}, nil
 }
 
-func NewProducerByConfig(config ProducerConfig) (*Producer, error) {
+func NewPublisherByConfig(config PublisherConfig) (*Publisher, error) {
 	channel, er1 := NewChannel(config.Url)
 	if er1 != nil {
 		return nil, er1
@@ -33,11 +33,11 @@ func NewProducerByConfig(config ProducerConfig) (*Producer, error) {
 	if er2 != nil {
 		return nil, er2
 	}
-	return NewProducer(channel, config.ExchangeName, config.Key, config.ContentType)
+	return NewPublisher(channel, config.ExchangeName, config.Key, config.ContentType)
 }
 
-func (p *Producer) Produce(ctx context.Context, data []byte, messageAttributes map[string]string) (string, error) {
-	opts := MapToTable(messageAttributes)
+func (p *Publisher) Publish(ctx context.Context, data []byte, attributes map[string]string) (string, error) {
+	opts := MapToTable(attributes)
 	msg := amqp.Publishing{
 		Headers:      opts,
 		DeliveryMode: amqp.Persistent,
@@ -49,10 +49,10 @@ func (p *Producer) Produce(ctx context.Context, data []byte, messageAttributes m
 	return "", err
 }
 
-func MapToTable(messageAttributes map[string]string) amqp.Table {
+func MapToTable(attributes map[string]string) amqp.Table {
 	opts := amqp.Table{}
-	if messageAttributes != nil {
-		for k, v := range messageAttributes {
+	if attributes != nil {
+		for k, v := range attributes {
 			opts[k] = v
 		}
 	}
